54dca4fa9c37cb24349fe81a8341af0eea9f3bce
nikhil8777 <nikhil@loyalistcollege.com>
2022-11-17 10:37:13 -0500

    mongoDb configuration completed

---
+++ sensor/connection_config/db_connection.py
@@ -0,0 +1,19 @@
+import pymongo
+from sensor.constants.database import DATABASE_NAME
+import certifi
+
+ca = certifi.where()
+
+class MongoDBConnection:
+    client = None
+    def __init__(self, db_name = DATABASE_NAME) -> None:
+        try:
+            if MongoDBConnection.client is None:
+                db_url = 'mongodb+srv://root:root@scaniatruck.3m5t41i.mongodb.net/?retryWrites=true&w=majority'
+                MongoDBConnection.client = pymongo.MongoClient(db_url,tlsCAFile=ca)
+            self.client = MongoDBConnection.client
+            self.db = self.client[db_name]
+            self.db_name = db_name
+
+        except Exception as e:
+            raise e
