84aa8751489f151b5b47ffa8d60df908b46456f9
nikhil8777 <nikhil@loyalistcollege.com>
2022-11-27 21:04:49 -0500

    Data Transformation artifacts configuration added

--- sensor/entity/artifacts.py
+++ sensor/entity/artifacts.py
@@ -2,5 +2,21 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestingArtifacts:
-    trained_file_path: str
-    test_file_path: str
+    training_file_path: str
+    testing_file_path: str
+
+@dataclass
+
+class DataValidationArtifacts:
+    validation_status : bool
+    valid_training_file_path : str
+    valid_testing_file_path : str
+    invalid_training_file_path : str
+    invalid_testing_file_path : str
+    drift_report_file_path : str
+
+class DataTransformationArtifacts:
+
+    transformed_object_file_path : str
+    transformed_training_file_path : str
+    transformed_test_file_path : str
