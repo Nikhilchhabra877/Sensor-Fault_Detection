af102a827ad74c96c75e94c71590f3644d4117b3
nikhil8777 <nikhil@loyalistcollege.com>
2022-11-30 19:19:06 -0500

    Metric code updated

--- sensor/MachineLearning/Metrics/classification_metric.py
+++ sensor/MachineLearning/Metrics/classification_metric.py
@@ -1,18 +1,19 @@
 from sensor.entity.artifacts import ClassificationMetricArtifacts
 from sensor.exception import CustomException
 from sensor.logger import logging
+import os,sys
 from sklearn.metrics import f1_score,precision_score,recall_score
 
 def get_classification_score(y_true,y_pred)->ClassificationMetricArtifacts:
 
     try:
-        f1_score = f1_score(y_true,y_pred)
-        precision_score = precision_score(y_true,y_pred)
-        recall_score = recall_score(y_true,y_pred)
+        model_f1_score = f1_score(y_true, y_pred)
+        model_recall_score = recall_score(y_true, y_pred)
+        model_precision_score=precision_score(y_true,y_pred)
 
-        results = ClassificationMetricArtifacts(f1_score=f1_score,precision_score=precision_score,recall_score=recall_score)
-
-        return results
-
+        classsification_metric =  ClassificationMetricArtifacts(f1_score=model_f1_score,
+                    precision_score=model_precision_score, 
+                    recall_score=model_recall_score)
+        return classsification_metric
     except Exception as e:
-        raise C
+        raise CustomException(e,sys)
