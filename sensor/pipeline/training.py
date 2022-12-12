433329b28e1576b8da62f76c393bcb3cd8df57f6
nikhil8777 <nikhil@loyalistcollege.com>
2022-12-12 00:54:44 -0500

    S3 Configuration added for syncing the artifacts

--- sensor/pipeline/training.py
+++ sensor/pipeline/training.py
@@ -10,6 +10,10 @@ from sensor.stages.data_transformation import DataTransformation
 from sensor.stages.model_trainer import ModelTrainer
 from sensor.stages.model_evaluation import ModelEvaluation
 from sensor.stages.model_pusher import ModelPusher
+from sensor.storage_s3.s3_sync import S3Sync
+
+from sensor.constants.S3_bucket import TRAINING_BUCKET,PREDICTION_BUCKET
+from sensor.constants.training_pipeline import SAVED_MODEL_DIR
 class TrainingPipeLine:
     is_pipeline_running = False
     def __init__(self):
@@ -21,6 +25,7 @@ class TrainingPipeLine:
         self.training_pipeline = training_pipeline
         self.data_validation_config = DataValidationConfig()
         self.model_trainer_config = ModelTrainerConfig(training_pipeline_config=training_pipeline)
+        self.s3_sync = S3Sync()
 
 
     def Data_Ingestion(self)->DataIngestingArtifacts:
@@ -113,6 +118,21 @@ class TrainingPipeLine:
         except  Exception as e:
             raise  CustomException(e,sys)
 
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET}/artifacts/{self.training_pipeline.timestamp}"
+            self.s3_sync.sync_dir_to_s3(folder = self.training_pipeline.artifacts_dir,aws_buket_url=aws_bucket_url)
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET}/{SAVED_MODEL_DIR}"
+            self.s3_sync.sync_dir_to_s3(folder = SAVED_MODEL_DIR,aws_buket_url=aws_bucket_url)
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
     def Run_Pipeline(self):
 
         try:
@@ -131,7 +151,11 @@ class TrainingPipeLine:
                 raise Exception("Trained model is not better than the best model")
             model_pusher_artifacts = self.Model_Push(model_eval_artifact)
             TrainingPipeLine.is_pipeline_running = False
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
         except Exception as e:
+            self.sync_artifact_dir_to_s3()
+            TrainingPipeLine.is_pipeline_running = False
             raise CustomException(e,sys)
 
 
